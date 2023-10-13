61af80a0fc12b724d51f0bce7dfd2d3b55e46014
kimjongha99 <jjhsun99@gmail.com>
2023-10-13 15:43:37 +0900

    .

--- second-service/src/main/java/com/gateway/secondservice/SecondServiceApplication.java
+++ second-service/src/main/java/com/gateway/secondservice/SecondServiceApplication.java
@@ -3,6 +3,7 @@ package com.gateway.secondservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SecondServiceApplication {
 
