3dfbe0df159c7b82a16c27bb2dd16ca89eb2379e
kimjongha99 <jjhsun99@gmail.com>
2023-10-16 15:48:22 +0900

    로드벨런싱

---
+++ eureka/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,37 @@
+package com.example.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    private  final Environment env;
+
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+
+        return env.getProperty("local.server.port");
+        }
+}
